6d3725e78aaad4f860ccaf9313cd074054e3f9e2
Nima Thing <nthing@verisk.com>
2020-06-09 22:33:18 +0545

    Initial Framework Ready

---
+++ NepaliTreat/gulpfile.js
@@ -0,0 +1,13 @@
+﻿/// <binding AfterBuild='default' />
+var gulp = require('gulp');
+var ugify = require("gulp-uglify");
+var concat = require("gulp-concat");
+
+gulp.task("minify", function () {
+  return gulp.src("wwwroot/js/**/*.js")
+    .pipe(ugify())
+    .pipe(concat("nepalitreat.min.js"))
+    .pipe(gulp.dest("wwwroot/dist"));
+});
+
+gulp.task('default', gulp.series("minify"), ["minify"]);
